28aad5b0cdcfea347ec5c79388dc7f7ba3d21463
Joscha Seelig <joscha-seelig@outlook.de>
2022-08-15 22:17:48 +0200

    improve reader test

--- test/bioit_exam/reader_test.clj
+++ test/bioit_exam/reader_test.clj
@@ -1,10 +1,12 @@
 (ns bioit-exam.reader-test
   (:require [clojure.test :refer [deftest testing is]]
-            [bioit-exam.io :refer :all]))
+            [bioit-exam.reader :refer :all]))
 
 (deftest read-fasta-test
   (testing "should parse fasta lines properly"
     (let [reads (parse-fasta [">Read_1" "AGTCAGTC" "TCAGTCAG"
                               ">Read_2" "CCC" "AGAGAGA"])]
-      (is (= 2 (count reads))))))
+      (is (= [{:header ">Read_1" :lines ["AGTCAGTC" "TCAGTCAG"]}
+              {:header ">Read_2" :lines ["CCC" "AGAGAGA"]}]
+             reads)))))
 
