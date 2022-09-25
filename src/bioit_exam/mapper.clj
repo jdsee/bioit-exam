33eb3792395db5351d5c46114616a89dc05800fa
Joscha Seelig <joscha-seelig@outlook.de>
2022-09-25 04:34:03 +0200

    Refactor and fix wrong format in kmer mapping
    Before I ordered the output in a wrong order.

--- src/bioit_exam/mapper.clj
+++ src/bioit_exam/mapper.clj
@@ -1,10 +1,13 @@
 (ns bioit-exam.mapper
-  (:require [bioit-exam.polisher :refer :all]
-            [clojure.algo.generic.functor :as f :only fmap]))
+  (:require [clojure.algo.generic.functor :as f :only fmap]
+            [clojure.spec.alpha :as s]))
 
-(defn compute-kmers [ref' kmerlen]
-  (->> ref'
-       (partition kmerlen 1)
+(defn compute-kmers
+  "Finds all unique kmers with length k and
+  maps them to matching postions in ref-seq."
+  [ref-seq k]
+  (->> ref-seq
+       (partition k 1)
        (map-indexed (fn [i kmer] {:pos i :kmer kmer}))
        (group-by :kmer)
        (f/fmap (partial map :pos))))
@@ -12,30 +15,65 @@
 (def kmer-positions
   (memoize compute-kmers))
 
-(defn get-kmer-positions [ref' kmer]
-  (-> ref'
-      (kmer-positions (count kmer))
-      (get kmer [])))
+(defn get-kmer-positions [ref-seq seed]
+  (-> ref-seq
+      (kmer-positions (count seed))
+      (get seed [])))
 
-(defn pad-with [fill-in xs]  (concat xs (repeat fill-in)))
+(defn pad-with [fill-in coll]
+  (concat coll (repeat fill-in)))
 
-(defn count-mismatches [ref' read' pos]
-  (->> ref'
-       (drop pos)
-       (take (count read'))
-       (pad-with nil)
-       (map = read')
+(defn diff-count [coll other]
+  (->> (pad-with nil coll)
+       (map = (pad-with nil other))
        (filter false?)
        (count)))
 
-(defn matching-positions [kmerlen max-mismatches ref' read']
-  (->> read'
-       (take kmerlen)
-       (get-kmer-positions ref')
-       (filter #(>= max-mismatches (count-mismatches ref' read' %)))))
+(defn tolerated?
+  "Return true if read-seq has max-div mismatches at max when
+  comparing to ref-seq at position pos."
+  [ref-seq read-seq pos max-div]
+  (->> (drop pos ref-seq)
+       (take (count read-seq))
+       (diff-count read-seq)
+       #(> % max-div)))
 
-(defn map-reads [kmerlen max-mismatches ref' reads]
-  (->> reads
-       (map (partial matching-positions kmerlen max-mismatches ref'))
-       (zipmap reads)))
+(defn matching-positions
+  "Find all positions in the given ref-seq at which kmers of
+  length k match kmers in read-seq with tolerance of max-div
+  mismatches."
+  [k max-div ref-seq read-seq]
+  (->> (take k read-seq)
+       (get-kmer-positions ref-seq)
+       (filter #(tolerated? ref-seq read-seq max-div %))))
 
+(defn group-by-pos [k max-div ref-seq]
+  (fn [mapping read-seq]
+    (->> (matching-positions k max-div ref-seq read-seq)
+         (reduce #(update %1 %2 concat [read-seq]) mapping))))
+         ; (map #(update mapping % concat [read-seq])))))
+
+(defn map-reads
+  "Maps all reads to ref-seq and returns a map of indices to the
+  reads with potential match to ref-seq at that index."
+  [k max-div ref-seq reads]
+  (reduce (group-by-pos k max-div ref-seq) {} reads))
+
+;; Following is not needed
+;; just experimenting a bit
+
+(s/fdef map-reads
+  :args (s/cat :k pos-int?
+               :max-div nat-int?
+               :ref-seq string?
+               :reads (s/coll-of string?))
+  :ret (s/map-of int? string?))
+
+(s/fdef matching-positions
+  :args (s/cat :k pos-int?
+               :max-div nat-int?
+               :ref-seq string?
+               :read-seq string?)
+  :ret (s/coll-of nat-int?)
+  :fn (s/and (s/coll-of nat-int?)
+             (fn [x] (every? #(< % (count (-> x :args :ref-seq))) (:ret x)))))
--- test/bioit_exam/mapper_test.clj
+++ test/bioit_exam/mapper_test.clj
@@ -7,23 +7,22 @@
     (let [ref' "CTGACT"
           reads ["TGAC"]
           mappings (map-reads 2 2 ref' reads)]
-      (is (= mappings {"TGAC" [1]}))))
+      (is (= mappings {1 ["TGAC"]}))))
 
   (testing "should map all positions where kmers match"
     (let [ref' "CTGACTG"
           reads ["CTG"]
           mappings (map-reads 3 2 ref' reads)]
-      (is (= mappings {"CTG" [0 4]}))))
+      (is (= mappings {0 ["CTG"], 4 ["CTG"]}))))
 
   (testing "should map reads when they have exactly the maximum of mismatches"
     (let [ref' "CTGACTG"
           reads ["TGAXTX"]
           mappings (map-reads 3 2 ref' reads)]
-      (is (= mappings {"TGAXTX" [1]}))))
+      (is (= mappings {1 ["TGAXTX"]}))))
 
   (testing "should not map reads when they exceed the maximum of mismatches"
     (let [ref' "CTGACTG"
           reads ["TGAXTX"]
           mappings (map-reads 3 3 ref' reads)]
-      (is (= mappings {"TGAXTX" [1]})))))
-
+      (is (= mappings {1 ["TGAXTX"]})))))
