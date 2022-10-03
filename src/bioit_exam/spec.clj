e6f0ff3f05bf93fa72e2a59369900e303eea54c6
Joscha Seelig <joscha-seelig@outlook.de>
2022-10-03 14:28:50 +0200

    Improve spec for domain model

--- src/bioit_exam/spec.clj
+++ src/bioit_exam/spec.clj
@@ -1,14 +1,17 @@
 (ns bioit-exam.spec
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::read
-  (s/and string? #(re-matches #"[ACGT]+" %)))
+(s/def ::base #(re-matches #"[ACGT]+" (str %)))
 
-(s/def ::reference ::read)
+(s/def ::read (s/and string? ::base))
+
+(s/def ::name string?)
+(s/def ::seq (s/and vector?
+                    (s/coll-of (s/and char? ::base))))
+(s/def ::reference (s/keys :req-un [::name ::seq]))
 
 (s/def ::mapped-reads
   (s/map-of nat-int? (s/coll-of ::read)))
 
 (s/def ::mapping
-  (s/and (s/keys :req [::reference ::mapped-reads])))
-
+  (s/keys :req-un [::reference ::mapped-reads]))
