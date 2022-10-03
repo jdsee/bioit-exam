623f0eb659443164bc10e2a560d143dd7410dce1
Joscha Seelig <joscha-seelig@outlook.de>
2022-10-03 16:26:33 +0200

    Refactor pileup writer for simplification

--- src/bioit_exam/writer.clj
+++ src/bioit_exam/writer.clj
@@ -2,12 +2,7 @@
   (:require
    [clojure.java.io :as io]
    [clojure.pprint :refer :all]
-   [clojure.spec.alpha :as s]
-   [clojure.spec.test.alpha :as stest]
    [clojure.string :as str]))
-
-(defn- base-repr [refbase base]
-  (if (= base refbase) \. base))
 
 (defn- make-line
   [{refname :name refseq :seq} i mapped-bases]
@@ -17,26 +12,26 @@
    :matches (count mapped-bases)
    :mapped-bases mapped-bases})
 
-(defn- line-partitioner [refbase]
-  (fn [[result remainder] [base & tail]]
-    [(str result (base-repr refbase base))
-     (if (some? tail)
-       (conj remainder tail)
-       remainder)]))
+(defn chop-matching-bases [refbase coll]
+  (reduce
+   (fn [[bases stash] [base & remain]]
+     [(str bases (str/replace base refbase \.))
+      (if (some? remain) (conj stash remain) stash)])
+   ["" []]
+   coll))
 
 (defn- pileup-bases
   [{:keys [mapped-reads] {refseq :seq} :ref} stash i]
   (->> (mapped-reads i)
        (concat stash)
-       (reduce (line-partitioner (get refseq i)) ["" []])))
+       (chop-matching-bases (get refseq i))))
 
 (defn make-pileup
   [{{refseq :seq :as reference} :ref :as mapping}]
-  (loop [stash "", accum [], i 0]
+  (loop [stash [], accum [], i 0]
     (let [[bases stash] (pileup-bases mapping stash i)
-          line (make-line reference i bases)
           next-pos (inc i)
-          accum (conj accum line)]
+          accum (conj accum (make-line reference i bases))]
       (if (> (count refseq) next-pos)
         (recur stash accum next-pos)
         accum))))
@@ -45,7 +40,7 @@
   [pileup]
   (map #(str/join \tab (vals %)) pileup))
 
-(defn write-pileup [filename pileup-lines]
+(defn write-pileup! [filename pileup-lines]
   (with-open [w (io/writer filename)]
     (doseq [line pileup-lines]
       (.write w line)
