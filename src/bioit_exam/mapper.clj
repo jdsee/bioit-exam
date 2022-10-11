3f03cca96eca18cd947090a957dd91458e26a638
Joscha Seelig <joscha-seelig@outlook.de>
2022-10-11 22:36:49 +0200

    Refactor mapper

--- src/bioit_exam/mapper.clj
+++ src/bioit_exam/mapper.clj
@@ -3,26 +3,25 @@
             [clojure.algo.generic.functor :as f :only fmap]
             [clojure.core.memoize :as memo :only memo]
             [clojure.core.reducers :as r]
-            [clojure.spec.alpha :as s]
-            [clojure.spec.test.alpha :as stest]))
+            [clojure.spec.alpha :as s]))
 
 (defn compute-kmers
   "Returns all unique kmers with length k mapped to the
-  starting postions at which they occur in refseq."
+  starting indices at which they occur in refseq."
   [refseq k]
   (->> refseq
        (partition k 1)
-       (map-indexed (fn [i kmer] {:pos i :kmer kmer}))
+       (map-indexed (fn [i kmer] {:i i :kmer kmer}))
        (group-by :kmer)
-       (f/fmap (partial map :pos))))
+       (f/fmap (partial map :i))))
 
-(def kmer-positions (memo/memo compute-kmers))
+(def kmer->indices (memo/memo compute-kmers))
 
-(defn get-kmer-positions
-  "Returns the positions at which kmer occurs in refseq"
+(defn get-kmer+indices
+  "Returns the indices at which kmer occurs in refseq"
   [refseq kmer]
-  (let [positions (kmer-positions refseq (count kmer))]
-    (get positions kmer)))
+  (let [indices (kmer->indices refseq (count kmer))]
+    (get indices kmer)))
 
 (defn pad-with [fill-in coll]
   (concat coll (repeat fill-in)))
@@ -35,7 +34,7 @@
 
 (defn tolerated?
   "Returns true if readseq has max-miss mismatches at most when
-  comparing to refseq at position i."
+  comparing to refseq at index i."
   [refseq readseq max-miss i]
   (->> (+ i (count readseq))
        (min (dec (count refseq)))
@@ -43,22 +42,22 @@
        (diff-count readseq)
        (>= max-miss)))
 
-(defn matching-positions
-  "Find all positions in the given refseq at which kmers of
-  length k match kmers in readseq with tolerance of max-miss
+(defn match-indices
+  "Find all indices in the given refseq at which a kmer of
+  length k matches a kmer in readseq with tolerance of max-miss
   mismatches.
   Be aware that the seed of length k has to match exactly on
   any kmer in readseq, otherwise the whole read is ignored."
   [k max-miss refseq readseq]
   (->> (take k readseq)
-       (get-kmer-positions refseq)
+       (get-kmer+indices refseq)
        (r/filter #(tolerated? refseq readseq max-miss %))))
 
-(defn- group-by-pos [positions]
+(defn- group-by-match-index [indices]
   (fn [mapping readseq]
     (r/reduce #(update %1 %2 conj readseq)
               mapping
-              (positions readseq))))
+              (indices readseq))))
 
 (defn concat-merge
   ([] {})
@@ -70,10 +69,10 @@
   [k max-miss {refseq :seq refname :name} reads]
   (let [refvec (vec refseq)
         readseqs (map :seq reads)
-        positions #(matching-positions k max-miss refvec %)]
+        indices #(match-indices k max-miss refvec %)]
     {:reference {:name refname, :seq refvec}
      :mapped-reads (r/fold concat-merge
-                           (group-by-pos positions)
+                           (group-by-match-index indices)
                            readseqs)}))
 
 ;; Following is not needed
@@ -82,32 +81,33 @@
 (s/fdef map-reads
   :args (s/cat :k pos-int?
                :max-miss nat-int?
-               :refseq :domain/reference
+               :refseq ::domain/reference
                :reads (s/coll-of :gen/base))
-  :ret :domain/mapping)
+  :ret ::domain/mapping)
 
-(s/fdef matching-positions
+(s/fdef match-indices
   :args (s/cat :k pos-int?
                :max-miss nat-int?
-               :refseq :domain/reference
-               :readseq :domain/read)
+               :refseq ::domain/reference
+               :readseq ::domain/read)
   :ret (s/coll-of nat-int?)
   :fn (s/and (s/coll-of nat-int?)
              (fn [x] (every? #(< % (count (-> x :args :refseq))) (:ret x)))))
 
 ;; PLAYGROUND
 
-(stest/instrument `map-reads)
-; (stest/instrument `matching-positions)
+(comment
+; (stest/instrument `map-reads)
+; (stest/instrument `match-indices)
 
 ;; NOTE: tolerance relates to the whole read, not to the kmer!
 
-(def reference
-  {:name "REF001"
-   :seq [\X \Y \Z \A \B \C \D \F \F \F \G \A \B \C \D \E \F \D \E \F \F]})
+  (def reference
+    {:name "REF001"
+     :seq [\X \Y \Z \A \B \C \D \F \F \F \G \A \B \C \D \E \F \D \E \F \F]})
 ;                  ^  ^  ^  ^  x  x  x              ^  ^  ^  x  ^
 ;                                          ^  ^  ^  ^  ^  x  ^
 
-(map-reads 3 1 reference ["ABCDEED" "DEFFE" "GDFE"])
+  (map-reads 3 1 reference ["ABCDEED" "DEFFE" "GDFE"])
 
-(matching-positions 4 1 "ABCDEFG" "ABCDDEFG")
+  (match-indices 4 1 "ABCDEFG" "ABCDDEFG"))
